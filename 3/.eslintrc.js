226ce974f82eed573073176cd3379799a4658bd2
heIgee <oleo7979@gmail.com>
2024-09-15 09:44:27 +0300

    Library MS - eslint & prettier

---
+++ 3/.eslintrc.js
@@ -0,0 +1,17 @@
+const { rules } = require('eslint-plugin-prettier');
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-expressions': 'off',
+  },
+};
